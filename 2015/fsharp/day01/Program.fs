2227f36b9d09a05a6233da5489da9de27882ab3c
jbpratt78 <jbpratt78@gmail.com>
2019-06-13 18:32:20 -0500

    d1p2 finished

--- 2015/fsharp/day01/Program.fs
+++ 2015/fsharp/day01/Program.fs
@@ -4,12 +4,11 @@ open System.IO
 [<EntryPoint>]
 let main argv =
     let data = File.ReadAllText("input")
-    let sum = ref 0
-    for c in data do
-        if c = '(' then
-            incr sum
-        else if c = ')' then
-            decr sum
-
-    printfn "%d" !sum
+    let p1 = Seq.sumBy (fun c -> if c = '(' then 1 else -1) data
+    let p2 = data 
+                |> Seq.map(fun d -> if d = '(' then 1 else -1)
+                |> Seq.scan (+) 0
+                |> Seq.findIndex (fun f -> f = -1)
+    printfn "Part one: %i" p1
+    printfn "Part two: %i" p2
     0
