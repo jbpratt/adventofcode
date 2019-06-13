fcd78a5a71f9bf192734a178dc9e2f436f6180a0
jbpratt78 <jbpratt78@gmail.com>
2019-06-13 16:30:20 -0500

    organize and start 2015 fsharp, in not a very fsharp way

---
+++ 2015/fsharp/day01/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let data = File.ReadAllText("input")
+    let sum = ref 0
+    for c in data do
+        if c = '(' then
+            incr sum
+        else if c = ')' then
+            decr sum
+
+    printfn "%d" !sum
+    0
