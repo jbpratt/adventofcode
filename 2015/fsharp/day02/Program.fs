ad0c4cebdc7aa26097aef388f763cd2e6b19737d
jbpratt78 <jbpratt78@gmail.com>
2019-06-13 19:08:04 -0500

    poor start to d2

--- 2015/fsharp/day02/Program.fs
+++ 2015/fsharp/day02/Program.fs
@@ -1,6 +1,16 @@
 ﻿open System
+open System.IO
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let calcSurfaceArea line = 
+        let splitLine (line : string) = line.Split 'x'
+
+        let calc (splitInts : string[]) = 
+            2*3*Int32.Parse(splitInts.[0]) + 2*4*Int32.Parse(splitInts.[1]) + 2*2*Int32.Parse(splitInts.[2])
+        
+        calc
+        // split line
+    let data = File.ReadAllLines("input")
+
     0 // return an integer exit code
