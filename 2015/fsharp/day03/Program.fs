887f4e8bdbf9ca83e03c4001186d3f73fd596a2e
jbpratt78 <jbpratt78@gmail.com>
2019-06-14 18:05:01 -0500

    d3p1 complete

---
+++ 2015/fsharp/day03/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let data = File.ReadAllText("input")
+
+    let p1 = data
+          |> Seq.map (fun c -> match c with | '>' -> (1,0) | '^' -> (0,1) | '<' -> (-1,0) | _ -> (0,-1))
+          |> Seq.scan (fun (x1,y1) (x2,y2) -> (x1 + x2, y1 + y2)) (0,0)
+          |> Seq.distinct
+          |> Seq.length
+          |> printfn "%i"
+    0
